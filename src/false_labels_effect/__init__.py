543fc745d69482d226fab24b56de9bb284d8dd9c
Daniel <dev.dc@pm.me>
2022-01-07 16:37:44 +0100

    initial commit - project setup

---
+++ src/false_labels_effect/__init__.py
@@ -0,0 +1,9 @@
+"""
+False Labels Effect
+
+Measuring the effect of false labels within training data on model performance
+"""
+__title__ = "false-labels-effect"
+__copyright__ = "© 2022 Alexander Thamm GmbH"
+
+from .version import __version__
---
+++ tests/test_util.py
@@ -0,0 +1,6 @@
+from false_labels_effect import util
+
+
+def test_get_resource_string():
+    s = util.get_resource_string('version.py')
+    assert '__version__ = ' in s
