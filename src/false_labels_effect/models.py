3d9364759b168c0b650c8a429d18719fd7f5236f
Daniel <dev.dc@pm.me>
2022-01-28 18:24:53 +0100

    add resnet boilerplate

--- src/false_labels_effect/models.py
+++ src/false_labels_effect/models.py
@@ -1,33 +1,64 @@
 from tensorflow.keras import layers, models
+from tensorflow.keras.applications import ResNet50
 
 
 # define model
-def create_cnn_model(shape):
+def create_model(img_shape, n_classes):
     """
     Create a basic CNN
 
     Parameters
     ----------
-    shape : tuple of two ints
+    img_shape : tuple of two ints
         shape of image
+    n_classes : int
+        number of classes
 
     Returns
     -------
-    cnn_model : keras.Sequential
-        CNN with three Conv2D and two Dense layers
+    model : keras.Sequential
+        cnn resnet model
     """
-    cnn_model = models.Sequential()
+    model = models.Sequential()
 
-    cnn_model.add(layers.Conv2D(244, (3, 3), activation='relu', input_shape=(shape[0], shape[1], 3)))
-    cnn_model.add(layers.MaxPooling2D((2, 2)))
-    cnn_model.add(layers.Conv2D(488, (3, 3), activation='relu', input_shape=(shape[0], shape[1], 3)))
-    cnn_model.add(layers.MaxPooling2D((2, 2)))
-    cnn_model.add(layers.Conv2D(978, (3, 3), activation='relu', input_shape=(shape[0], shape[1], 3)))
+    model.add(layers.Conv2D(244, (3, 3), activation='relu', input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(488, (3, 3), activation='relu', input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(978, (3, 3), activation='relu', input_shape=(img_shape[0], img_shape[1], 3)))
 
-    cnn_model.add(layers.Flatten())
-    cnn_model.add(layers.Dense(64, activation='relu'))
-    cnn_model.add(layers.Dense(4, activation='softmax'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(n_classes, activation='softmax'))
 
-    # print(cnn_model.summary())
+    print(model.summary())
 
-    return cnn_model
+    return model
+
+
+def create_resnet_model(img_shape, n_classes):
+    """
+    Create a CNN based on ResNet50
+
+    Parameters
+    ----------
+    n_classes : int
+        number of classes
+
+    Returns
+    -------
+    model : keras.Sequential
+        cnn resnet model
+    """
+    resnet_model = ResNet50(include_top=False, weights="imagenet", input_tensor=img_shape)
+
+    model = models.Sequential()
+
+    model.add(resnet_model)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dense(2048, activation="relu"))
+    model.add(layers.Dense(n_classes, activation='softmax'))
+
+    print(model.summary())
+
+    return model
