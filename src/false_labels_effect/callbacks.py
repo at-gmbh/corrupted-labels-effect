1b21effb5871a5e315a566ad02f778468c8cbcb2
Daniel <dev.dc@pm.me>
2022-02-18 19:31:49 +0100

    add callback to log classification report

---
+++ src/false_labels_effect/callbacks.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.metrics import classification_report
+from tensorflow.keras.callbacks import Callback
+
+
+class class_report_cb(Callback):
+    """
+    Calculates and logs classification report for validatiot
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+
+    def on_epoch_end(self, epoch, logs=None):
+        print('self.model')
+        print(self.model)
+    #     y_pred = self.model.predict(self.x_val)
+    #     report = classification_report(self.y_val, y_pred)
+    #     logs['classification_report'] = report
+    #     print(report)
+
+        return
