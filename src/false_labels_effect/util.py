bc3dd6e7fab4bb4027836addcb15488a7be24b00
Daniel <dev.dc@pm.me>
2022-02-25 19:25:01 +0100

    refactor to reuse false labels in higher ratios

--- src/false_labels_effect/util.py
+++ src/false_labels_effect/util.py
@@ -6,7 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_labels(npy_path):
@@ -25,7 +25,6 @@ def load_labels(npy_path):
     -------
     TYPE
         dict[dict[str => Any]].
-
     """
     labels_dict = np.load(npy_path, allow_pickle=True).item()
 
@@ -59,9 +58,9 @@ def select_label(labels_dict: dict, task: str):
         if task in ['Class', 'Annotation']:
             new_dict[image_id] = list_of_dicts[task]
         elif task == 'Subclass':
-            new_dict[image_id] = [list_of_dicts['Class'], list_of_dicts['Subclass']]
+            new_dict[image_id] = f'{list_of_dicts["Class"]} - {list_of_dicts["Subclass"]}'
         else:
-            print("Selected model task labels not existing.")
+            print("Selected model task does not exist")
             sys.exit()
     
     return new_dict
@@ -84,7 +83,7 @@ def encode_labels(train_labels_dict: dict, test_labels_dict:dict):
             Dictionary of observation ID and numerical label
     """
     encoded_dicts = []
-    le = preprocessing.LabelEncoder()
+    le = LabelEncoder()
     
     # fit Encoder to labels from test and train to include edge cases
     y = list(train_labels_dict.values()) + list(test_labels_dict.values())
@@ -117,7 +116,6 @@ def reformat_labels(labels_dict):
     new_dict : dict[str => dict[str => Any]]
         Dictionary where each item is one dictionary containing all key-value
         pairs from the inner list of input dictionaries.
-
     """
     new_dict = dict()
     for image_id, list_of_dicts in labels_dict.items():
@@ -152,7 +150,6 @@ def resize(image: Image, labels_dict: dict, new_size: tuple):
         labels dict with rescaled Annotations polygon and the tuple used for
         scaling stored in key "Scale". Use the latter to scale a prediction
         for this image back up into its original format.
-
     """
     img_res = image.resize(new_size)
     labels_ret = labels_dict.copy()
@@ -195,37 +192,110 @@ def train_val_split(x_train, y_train, val_split):
     """
     # from train img randomly select img for validation
     x_val = random.sample(list(x_train), math.floor(len(x_train) * val_split))
+
     # remove val img from train img
     x_train_splitted = [id for id in x_train if id not in x_val]
 
+    # from train labels select respective val img labels
     y_val = {}
     y_train_splitted = y_train.copy()
-
     for id in x_val:
         y_val[id] = y_train[id]
         y_train_splitted.pop(id)
     
     return x_train_splitted, y_train_splitted, x_val, y_val
 
-def make_false_labels(label_path, labels, false_labels_ratio, classes):
+def make_false_labels(label_path, labels, fals_label_ratios, classes):
     """
-    Randomly changes the label values in labels according to false_labels_ratio.
+    Randomly changes the label values in labels according to false_labels_ratio
+    and keeps the same false labels for higher ratios
 
     Parameters
     ----------
     label_path : str
-        path to save or load existing false labels from
-    labels : dict[ str => str ]
-        true labels
-    false_labels_ratio : float
-        Ratio of labels to make false between 0 and 1
+        path to save false labels to
+    labels : dict[ str => int ]
+        true labels with IDs
+    false_label_ratios : list[float]
+        List of ratios of labels to make false between 0 and 1
         Resulting number of labels will be rounded down (math.floor)
     classes : int
         number of classes in data set
     
     Returns
     -------
-        numpy array with wrong labels
+    None
+    """
+    updated_labels = labels.copy()
+    n_labels = len(labels)
+    ratio_ids_dict = {}
+
+    for ratio in fals_label_ratios:
+        label_name = f'Train_{format(int(ratio*10000),"06d")}r.npy'
+
+        # for first (lowest) ratio, create false ratios
+        if len(ratio_ids_dict.keys()) == 0:
+            # calculate number of false labels
+            n_labels_needed = math.floor(ratio * n_labels)
+            
+            # generate the IDs that will have false label
+            ratio_ids_dict[ratio] = random.sample(list(labels.keys()),
+                                                n_labels_needed)
+
+            # generate false labels
+            updated_labels = generate_false_labels(ratio_ids_dict[ratio],
+                                                labels,
+                                                classes)
+        
+        # for following ratios, include previous false label
+        else:
+            # get previous highest ratio and corresponding label IDs
+            prev_max_ratio = max(ratio_ids_dict.keys())
+            false_label_ids_existing =  ratio_ids_dict[prev_max_ratio]
+
+            # calculate number of false labels still needed to fullfill ratio
+            n_labels_needed = math.floor(ratio * n_labels) \
+                            - len(false_label_ids_existing)
+
+            # filter out already existing false label IDs
+            false_label_ids_unused = [item for item in labels.keys() \
+                                    if item not in false_label_ids_existing]
+
+            # generate missing IDs that will have false label
+            false_label_ids_new = random.sample(list(false_label_ids_unused),
+                                                n_labels_needed)
+            
+            # combine existing and new false label ids
+            ratio_ids_dict[ratio] = false_label_ids_new
+
+            # generate false labels
+            updated_labels = generate_false_labels(ratio_ids_dict[ratio],
+                                                updated_labels,
+                                                classes)
+
+        # save np array
+        np.save(f'{label_path}\{label_name}', updated_labels)
+
+    return
+
+def generate_false_labels(label_ids, labels, classes):
+    """
+    Falsifies given labels
+
+    Parameters
+    ----------
+    label_ids : list[str]
+        list of IDs which labels to be falsified
+    labels : dict[ str => int ]
+        true labels with IDs
+    classes : int
+        number of classes in data set
+
+    Returns
+    -------
+    falsified_labels : np.array
+        array of labels including falsified labels
+    
     Sample:
         print(y_train)
         >>> {'Train_00001': 0,
@@ -233,51 +303,55 @@ def make_false_labels(label_path, labels, false_labels_ratio, classes):
         >>>  'Train_00003': 2,
         >>>  'Train_00004': 0,
         >>>  'Train_00005': 2}
-        print(make_false_labels(y_train, 0.5))
+        print(generate_false_labels(false_label_ids, y_train, 0.2))
         >>> {'Train_00001': 0,
         >>>  'Train_00002': 0,
         >>>  'Train_00003': 2,
         >>>  'Train_00004': 3,
         >>>  'Train_00005': 0}
     """
-    # check if false labels already exist for specified ratio
-    label_name = f'Train_{format(int(false_labels_ratio*10000),"04d")}.npy'
+    falsified_labels = labels.copy()
+    
+    for id in label_ids:
+        # init wrong label
+        wrong_label = None
 
-    if not os.path.exists(f'{label_path}\{label_name}'):
-        false_labels = labels
-        n_labels = len(labels)
+        # get true value
+        true_value = labels[id]
 
-        # Generate the IDs that will have false label
-        false_labels_count = math.floor(n_labels * false_labels_ratio)
-        print("Number of false labels:", false_labels_count)
-        wrong_label_keys = random.sample(list(labels.keys()), false_labels_count)
+        # change label until label is wrong
+        while wrong_label == None or wrong_label == true_value:
+            wrong_label = random.randint(0, classes - 1)
 
-        # Manipulate label if index in wrong_label_ids
-        for key in wrong_label_keys:
-            # init wrong label
-            wrong_label = None
+        # set wrong label
+        falsified_labels[id] = wrong_label
+    
+    return falsified_labels
 
-            # get true value
-            true_value = labels[key]
+def load_false_labels(label_path, false_labels_ratio):
+    """
+    Load labels including falsified labels corresponding to given ratio
 
-            # change label until label is wrong
-            while wrong_label == None or wrong_label == true_value:
-                wrong_label = random.randint(0, classes - 1)
+    Parameters
+    ----------
+    label_path : str
+        path to save or load existing false labels from
+    false_labels_ratio : float
+        Ratios of false labels
 
-            # set wrong label
-            false_labels[key] = wrong_label
-        
-        np.save(f'{label_path}\{label_name}', false_labels)
-        return false_labels
-    # else load existing false labels
-    else:
-        false_labels = np.load(f'{label_path}\{label_name}',
-                               allow_pickle=True).item()
-        n_labels = len(false_labels)
-        false_labels_count = math.floor(n_labels * false_labels_ratio)
-        print("Number of false labels:", false_labels_count)
+    Returns
+    -------
+    false_labels : np.array
+        array of train labels including falsified labels
+    """
+    label_name = f'Train_{format(int(false_labels_ratio*10000),"06d")}r.npy'
+    false_labels = np.load(f'{label_path}\{label_name}',
+                            allow_pickle=True).item()
+    n_labels = len(false_labels)
+    false_labels_count = math.floor(n_labels * false_labels_ratio)
+    print("Number of false labels:", false_labels_count)
 
-        return false_labels
+    return false_labels
 
 
 def plot_sample(image: np.ndarray, labels: dict):
@@ -298,7 +372,6 @@ def plot_sample(image: np.ndarray, labels: dict):
     Returns
     -------
     None.
-
     """
 
     plt.imshow(image)
