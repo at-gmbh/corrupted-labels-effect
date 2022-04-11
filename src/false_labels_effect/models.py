5125e48c5929be0429305bbf01473db51330bb13
Daniel Czwalinna <dev.dc@pm.me>
2022-04-11 16:44:49 +0200

    update basic cnn architecture

--- src/false_labels_effect/models.py
+++ src/false_labels_effect/models.py
@@ -26,6 +26,7 @@ def create_cnn_model(img_shape, n_classes, false_labels_ratio):
     pool_size = (2, 2)
     leaky_alpha = 0.3
     dropout_ratio_conv = 0.1
+    dropout_ratio_dense = 0.1
 
     model = models.Sequential()
 
@@ -35,59 +36,84 @@ def create_cnn_model(img_shape, n_classes, false_labels_ratio):
                             input_shape=(img_shape[0], img_shape[1], 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=leaky_alpha))
-    model.add(layers.Dropout(dropout_ratio_conv))
+    model.add(layers.Conv2D(32,
+                            kernel_tupel,
+                            activation=activation_func,
+                            input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=leaky_alpha))
     model.add(layers.MaxPooling2D(pool_size))
 
+    model.add(layers.Dropout(dropout_ratio_conv))
     model.add(layers.Conv2D(64,
                             kernel_tupel,
                             activation=activation_func,
                             input_shape=(img_shape[0], img_shape[1], 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=leaky_alpha))
-    model.add(layers.Dropout(dropout_ratio_conv))
+    model.add(layers.Conv2D(64,
+                            kernel_tupel,
+                            activation=activation_func,
+                            input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=leaky_alpha))
     model.add(layers.MaxPooling2D(pool_size))
 
+    model.add(layers.Dropout(dropout_ratio_conv))
     model.add(layers.Conv2D(128,
                             kernel_tupel,
                             activation=activation_func,
                             input_shape=(img_shape[0], img_shape[1], 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=leaky_alpha))
-    model.add(layers.Dropout(dropout_ratio_conv))
+    model.add(layers.Conv2D(128,
+                            kernel_tupel,
+                            activation=activation_func,
+                            input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=leaky_alpha))
     model.add(layers.MaxPooling2D(pool_size))
 
+    model.add(layers.Dropout(dropout_ratio_conv))
     model.add(layers.Conv2D(256,
                             kernel_tupel,
                             activation=activation_func,
                             input_shape=(img_shape[0], img_shape[1], 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=leaky_alpha))
-    model.add(layers.Dropout(dropout_ratio_conv))
     model.add(layers.MaxPooling2D(pool_size))
 
-    model.add(layers.Conv2D(256,
-                            kernel_tupel,
-                            activation=activation_func,
-                            input_shape=(img_shape[0], img_shape[1], 3)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU(alpha=leaky_alpha))
     model.add(layers.Dropout(dropout_ratio_conv))
-    model.add(layers.MaxPooling2D(pool_size))
-
     model.add(layers.Conv2D(512,
                             kernel_tupel,
                             activation=activation_func,
                             input_shape=(img_shape[0], img_shape[1], 3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=leaky_alpha))
+    model.add(layers.MaxPooling2D(pool_size))
+
     model.add(layers.Dropout(dropout_ratio_conv))
+    model.add(layers.Conv2D(1024,
+                            kernel_tupel,
+                            activation=activation_func,
+                            input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=leaky_alpha))
     model.add(layers.MaxPooling2D(pool_size))
 
     model.add(layers.Flatten())
 
-    model.add(layers.Dense(1024))
+    model.add(layers.Dropout(dropout_ratio_dense))
+    model.add(layers.Dense(512))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU(alpha=leaky_alpha))
+
+    model.add(layers.Dropout(dropout_ratio_dense))
+    model.add(layers.Dense(256))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=leaky_alpha))
+
+    model.add(layers.Dropout(dropout_ratio_dense))
     model.add(layers.Dense(n_classes, activation='softmax'))
 
     model._name=f'basic_{format(int(false_labels_ratio*10000),"05d")}r_{n_classes}c'
